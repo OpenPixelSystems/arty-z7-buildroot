458fd0a2ac968629be2cfa37d5b789ffea8c14ff
Bram Vlerick <bram.vlerick@openpixelsystems.org>
2019-04-24 15:04:45 +0200

    Copy dtb to output/images/
    Signed-off-by: Bram Vlerick <bram.vlerick@openpixelsystems.org>

--- package/zynq-firmware/zynq-firmware.mk
+++ package/zynq-firmware/zynq-firmware.mk
@@ -11,6 +11,7 @@ ZYNQ_FIRMWARE_SITE = $(call github,openpixelsystems,makefile-vivado-project,mast
 define ZYNQ_FIRMWARE_BUILD_CMDS
     REPO_DIR=$(BR2_ZYNQ_REPO_PATH) ORIGIN_DIR=$(BR2_ZYNQ_FIRMWARE_PATH) $(MAKE) CC="$(HOST_CC)" LD="$(HOST_LD)" -C $(@D)
     cp $(@D)/build/project.sdk/project.bit $(BINARIES_DIR)/fpga.bit 
+    cp $(@D)/system-ops.dtb $(BINARIES_DIR)/devicetree.dtb 
 endef
 
 $(eval $(generic-package))
